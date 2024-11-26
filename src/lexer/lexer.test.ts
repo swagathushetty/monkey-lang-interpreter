b911390ae91d9e4aa682797260bcac5a97b4ab91
swagathushetty <100142687+swagath-badbeat@users.noreply.github.com>
2024-11-26 20:30:08 +0530

    cleaning up

--- src/lexer/lexer.test.ts
+++ src/lexer/lexer.test.ts
@@ -17,43 +17,6 @@ describe('lexer',()=>{
                 5 < 10 > 5;
         `;
         const tests = [
-            // {expectedType:TokenTypes.LET,expectedLiteral:"let"},
-            // {expectedType:TokenTypes.IDENT,expectedLiteral:"five"},
-            // {expectedType:TokenTypes.ASSIGN,expectedLiteral:"="},
-            // {expectedType:TokenTypes.INT,expectedLiteral:"5"},
-            // {expectedType:TokenTypes.SEMICOLON,expectedLiteral:";"},
-            // {expectedType:TokenTypes.LET,expectedLiteral:"let"},
-            // {expectedType:TokenTypes.IDENT,expectedLiteral:"ten"},
-            // {expectedType:TokenTypes.ASSIGN,expectedLiteral:"="},
-            // {expectedType:TokenTypes.INT,expectedLiteral:"10"},
-            // {expectedType:TokenTypes.SEMICOLON,expectedLiteral:";"},
-            // {expectedType:TokenTypes.LET,expectedLiteral:"let"},
-            // {expectedType:TokenTypes.IDENT,expectedLiteral:"result"},
-            // {expectedType:TokenTypes.ASSIGN,expectedLiteral:"="},
-            // {expectedType:TokenTypes.FUNCTION,expectedLiteral:"fn"},
-            // {expectedType:TokenTypes.LPAREN,expectedLiteral:"("},
-            // {expectedType:TokenTypes.IDENT,expectedLiteral:"x"},
-            // {expectedType:TokenTypes.COMMA,expectedLiteral:","},
-            // {expectedType:TokenTypes.IDENT,expectedLiteral:"y"},
-            // {expectedType:TokenTypes.RPAREN,expectedLiteral:")"},
-            // {expectedType:TokenTypes.LBRACE,expectedLiteral:"{"},
-            // {expectedType:TokenTypes.IDENT,expectedLiteral:"x"},
-            // {expectedType:TokenTypes.PLUS,expectedLiteral:"+"},
-            // {expectedType:TokenTypes.IDENT,expectedLiteral:"y"},
-            // {expectedType:TokenTypes.SEMICOLON,expectedLiteral:";"},
-            // {expectedType:TokenTypes.RBRACE,expectedLiteral:"}"},
-            // {expectedType:TokenTypes.SEMICOLON,expectedLiteral:";"},
-            // {expectedType:TokenTypes.LET,expectedLiteral:"let"},
-            // {expectedType:TokenTypes.IDENT,expectedLiteral:"result"},
-            // {expectedType:TokenTypes.ASSIGN,expectedLiteral:"="},
-            // {expectedType:TokenTypes.IDENT,expectedLiteral:"add"},
-            // {expectedType:TokenTypes.LPAREN,expectedLiteral:"("},
-            // {expectedType:TokenTypes.IDENT,expectedLiteral:"5"},
-            // {expectedType:TokenTypes.COMMA,expectedLiteral:","},
-            // {expectedType:TokenTypes.IDENT,expectedLiteral:"10"},
-            // {expectedType:TokenTypes.RPAREN,expectedLiteral:")"},
-            // {expectedType:TokenTypes.SEMICOLON,expectedLiteral:";"},
-            // {expectedType:TokenTypes.EOF,expectedLiteral:""},
             { expectedType: TokenTypes.LET, expectedLiteral: "let" },
             { expectedType: TokenTypes.IDENT, expectedLiteral: "five" },
             { expectedType: TokenTypes.ASSIGN, expectedLiteral: "=" },
@@ -106,28 +69,6 @@ describe('lexer',()=>{
 
 
             {expectedType:TokenTypes.EOF,expectedLiteral:""},
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
         ];
 
