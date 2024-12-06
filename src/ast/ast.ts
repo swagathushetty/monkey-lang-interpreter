707c59403712da22d17e5de5f771a951fb06dd84
swagathushetty <swagath.shetty@gmail.com>
2024-12-06 09:53:30 +0530

    added empty implementation for let statement and identifier

--- src/ast/ast.ts
+++ src/ast/ast.ts
@@ -1,10 +1,24 @@
+import { Token } from "../token/token";
+
 interface ASTNode {
     tokenLiteral():string;
 }
 
-interface Statement extends ASTNode {}
+interface Statement extends ASTNode {
+    statementNode():void
+}
 
-interface Expression extends ASTNode {}
+interface Expression extends ASTNode {
+    expressionNode():void
+}
+
+//let x = 5
+
+//tree structure
+//program
+    // let -> statement
+        // x= identifier 
+        // 5 expression
 
 class Program implements ASTNode {
     private statements: Statement[];
@@ -22,3 +36,41 @@ class Program implements ASTNode {
         return ""
     }
 } 
+
+class LetStatement implements Statement {
+    private token:Token
+    private name: Identifier
+    private value:Expression
+
+    constructor(token:Token,name:Identifier,value:Expression){
+        this.token = token
+        this.name = name
+        this.value = value
+    }
+
+    public statementNode(): void {
+        
+    }
+
+    public tokenLiteral(): string {
+        return this.token.literal
+    }
+}
+
+class Identifier implements Expression {
+    private token:Token
+    private value:Expression
+
+    constructor(token:Token,value:Expression){
+        this.token = token
+        this.value = value
+    }
+
+    public expressionNode(): void {
+        
+    }
+
+    public tokenLiteral(): string {
+        return this.token.literal
+    }
+}
